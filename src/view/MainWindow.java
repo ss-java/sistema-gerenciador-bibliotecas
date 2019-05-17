a4875b917164a70f9de8d3f286262aff9302a561
Paulo Trindade <paulo7trindade@gmail.com>
2019-05-17 19:41:17 +0000

    Exit button add to the screen

--- src/view/MainWindow.java
+++ src/view/MainWindow.java
@@ -10,6 +10,11 @@ import java.awt.EventQueue;
 import java.awt.FlowLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JEditorPane;
+import javax.swing.JList;
+import javax.swing.JTextArea;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class MainWindow extends JFrame {
 
@@ -34,6 +39,11 @@ public class MainWindow extends JFrame {
 	private JButton btnCustomers;
 	
 	/**
+	 * Button that close the application.
+	 */
+	private JButton btnExit;
+	
+	/**
 	 * Open the window.
 	 */
 	public static void open() {
@@ -54,28 +64,37 @@ public class MainWindow extends JFrame {
 	 */
 	public MainWindow() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 830, 506);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new FlowLayout(FlowLayout.LEADING, 5, 5));
 		setContentPane(contentPane);
 
-		btnBooksLoan = new JButton("Empréstimos");
+		btnBooksLoan = new JButton("Empr\u00E9stimos");
+		btnBooksLoan.setBounds(12, 10, 115, 36);
 		btnBooksLoan.setName("btnBooksLoan");
 		
 		btnBooks = new JButton("Livros");
+		btnBooks.setBounds(150, 10, 84, 36);
 		btnBooks.setName("btnBooks");
 		
 		btnCustomers = new JButton("Clientes");
+		btnCustomers.setBounds(271, 10, 92, 36);
 		btnCustomers.setName("btnCustomers");
-
+		
+		btnExit = new JButton("Close Application");
+		btnExit.setBounds(401, 10, 175, 36);
+		btnExit.setName("btnExit");
+				
 		btnBooksLoan.addActionListener(new HandleButtonClick());
 		btnBooks.addActionListener(new HandleButtonClick());
 		btnCustomers.addActionListener(new HandleButtonClick());
+		btnExit.addActionListener(new HandleButtonClick());
+		contentPane.setLayout(null);
 
 		contentPane.add(btnBooksLoan);
 		contentPane.add(btnBooks);
 		contentPane.add(btnCustomers);
+		contentPane.add(btnExit);
 	}
 
 	/**
@@ -84,7 +103,7 @@ public class MainWindow extends JFrame {
 	private class HandleButtonClick implements ActionListener {
 		public void actionPerformed(ActionEvent event) {
 			JButton clickedButton = ((JButton) event.getSource());
-			System.out.println("Você clicou em: " + clickedButton.getName());
+			System.out.println("Voc� clicou em: " + clickedButton.getName());
 			
 			if (clickedButton.getName() == "btnBooks") {
 				BooksWindow.open();
@@ -97,7 +116,10 @@ public class MainWindow extends JFrame {
 			else if (clickedButton.getName() == "btnBooksLoan") {
 				LoansWindow.open();
 			}
+			
+			else if (clickedButton.getName() == "btnExit") {
+				System.exit(0);
+			}
 		}
 	}
-
 }
