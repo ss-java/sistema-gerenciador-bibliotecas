c0e60d2ae4d4fed2e6ea43ea1b431d2b6f0381f8
MarcosHGA <marcos111516@gmail.com>
2019-05-30 22:25:39 -0300

    Adicionando table

--- src/view/CustomersWindow.java
+++ src/view/CustomersWindow.java
@@ -19,6 +19,13 @@ import model.DatabaseConnection;
 
 import javax.swing.JTextField;
 import javax.swing.JLabel;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.GridLayout;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.table.DefaultTableModel;
 
 public class CustomersWindow extends JFrame {
 
@@ -27,6 +34,7 @@ public class CustomersWindow extends JFrame {
 	 */
 	private JPanel windowPanel;
 	private JTextField textField;
+	private JTable table;
 	
 	/**
 	 * Open the window.
@@ -50,53 +58,87 @@ public class CustomersWindow extends JFrame {
 	 */
 	public CustomersWindow() {
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 643, 530);
+		setBounds(100, 100, 800, 600);
 		windowPanel = new JPanel();
 		windowPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(windowPanel);
 				
 		textField = new JTextField();
-		textField.setBounds(10, 54, 447, 20);
-		windowPanel.add(textField);
 		textField.setColumns(10);
 		
-		JButton btnNewButton = new JButton("Cadastrar");
-		btnNewButton.setBounds(269, 438, 161, 38);
-		btnNewButton.addActionListener(new ActionListener() {
+		JLabel lblNewLabel = new JLabel("Nome do Cliente:");
+		
+		JButton btnExcluir = new JButton("Excluir");
+		
+		JButton btnEditar = new JButton("Editar");
+		
+		JButton btnAdicionar = new JButton("Adicionar");
+		
+		JButton btnSalvar = new JButton("Salvar");
+		
+		table = new JTable();
+		
+		btnSalvar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
-					if(textField.getText().isEmpty() || textField.getText().startsWith(" ")) {
+					if(textField.getText().isEmpty()) {
 						JOptionPane.showMessageDialog(null, "O nome do Cliente não pode estar vazio");
 						throw new Exception("O nome Cliente vazio");	
 					}				
-					else	{
-						
-						
+					else	{												
 						new CustomerController().createCustomer(textField.getText());  
 						Connection connection = DatabaseConnection.getInstance().getConnection();
 						Statement statement = connection.createStatement();
-						ResultSet rs = statement.executeQuery("Select * from customers");
-						while(rs.next()) {
+						ResultSet rs = statement.executeQuery("Select name from customers");
+						while(rs.next()) {							
 							String t = rs.getString("name");
 							System.out.println(t);
 						}
 						System.out.println(statement.getResultSet().toString());
 						JOptionPane.showMessageDialog(null, "Cliente Cadastrado com sucesso\nCliente: " + textField.getText());
-					}
-					
+					}										
 				}catch(Exception ex) {
 					System.err.println(ex.getMessage());
 				}
-
 			}
-		});
-		windowPanel.setLayout(null);
-		windowPanel.add(btnNewButton);
-		
-		JLabel lblNewLabel = new JLabel("Nome Cliente:");
-		lblNewLabel.setBounds(10, 35, 92, 14);
-		windowPanel.add(lblNewLabel);
-		
-		
+		});		
+		table.setToolTipText("");
+		GroupLayout gl_windowPanel = new GroupLayout(windowPanel);
+		gl_windowPanel.setHorizontalGroup(
+			gl_windowPanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_windowPanel.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_windowPanel.createParallelGroup(Alignment.LEADING, false)
+						.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 182, GroupLayout.PREFERRED_SIZE)
+						.addGroup(gl_windowPanel.createSequentialGroup()
+							.addComponent(btnSalvar, GroupLayout.PREFERRED_SIZE, 106, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(btnAdicionar, GroupLayout.PREFERRED_SIZE, 106, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(btnEditar, GroupLayout.PREFERRED_SIZE, 93, GroupLayout.PREFERRED_SIZE)
+							.addGap(6)
+							.addComponent(btnExcluir, GroupLayout.PREFERRED_SIZE, 87, GroupLayout.PREFERRED_SIZE))
+						.addComponent(textField))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(table, GroupLayout.DEFAULT_SIZE, 344, Short.MAX_VALUE))
+		);
+		gl_windowPanel.setVerticalGroup(
+			gl_windowPanel.createParallelGroup(Alignment.TRAILING)
+				.addGroup(Alignment.LEADING, gl_windowPanel.createSequentialGroup()
+					.addGap(6)
+					.addComponent(lblNewLabel)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED, 467, Short.MAX_VALUE)
+					.addGroup(gl_windowPanel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnEditar, GroupLayout.PREFERRED_SIZE, 33, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnExcluir, GroupLayout.PREFERRED_SIZE, 33, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnAdicionar, GroupLayout.PREFERRED_SIZE, 33, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnSalvar, GroupLayout.PREFERRED_SIZE, 33, GroupLayout.PREFERRED_SIZE)))
+				.addGroup(Alignment.LEADING, gl_windowPanel.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(table, GroupLayout.DEFAULT_SIZE, 540, Short.MAX_VALUE))
+		);
+		windowPanel.setLayout(gl_windowPanel);
 	}
 }
